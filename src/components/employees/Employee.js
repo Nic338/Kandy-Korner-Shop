c9417ab0389bcbe55288bd38a012c2f1bef3e79a
Nicholas Myers <nic338@gmail.com>
2023-04-19 16:15:14 -0400

    finished ch18

--- src/components/employees/Employee.js
+++ src/components/employees/Employee.js
@@ -1,15 +1,39 @@
 // import { Link } from "react-router-dom"
+import "./employees.css"
 
-// export const Employee = ({id, name, email}) => {
-//     //this is built by the EmployeeList component
-//     //changed the name to a react router link, reroute the browser to employees/"the employee id"
-//     return <section className="employee">
-//     <div>
-//         <Link to={`/employees/${id}`}>Name: {name} </Link>
-//     </div>
-//     <div>Email: {email}</div>
-// </section>
-// }
+export const Employee = ({employeeObject, getAllEmployees}) => {
+   
+const FireEmployee = () => {
+    return <button onClick={() => {
+        fetch(`http://localhost:8088/users/${employeeObject.userId}`, {
+            method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    isStaff: false
+                })
+        })
+        .then(fetch(`http://localhost:8088/employees/${employeeObject.id}`, {
+                method: "DELETE"
+            }))
+        .then(() => {
+            getAllEmployees()
+        })
+    }}>Fire Employee</button>
+ }
+
+    //this is built by the EmployeeList component
+    
+    return <section className="employee" key={employeeObject.id}>
+    <header className="employee__header">{employeeObject?.user?.name}</header>
+<div>Start Date:  {employeeObject?.startDate}</div>
+<div>Pay Rate:  {employeeObject?.payRate}</div>
+    <footer className="employee__footer">Currently working at {employeeObject?.location?.address}</footer>
+    {FireEmployee()}
+    </section>
+}
 
 
 // WE DONT NEED THIS FOR THIS EXERCISE
+// I LIED I EVENTUALLY DID NEED THIS
--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -14,18 +14,22 @@ export const EmployeeList = () => {
             })
     },[]
     )
+const getAllEmployees = () => {
+    fetch(`http://localhost:8088/employees?_expand=location&_expand=user`)
+    .then(response => response.json())
+    .then((employeeArray) => {
+        setEmployees(employeeArray)
+    })
+
+}
+   
     return <article className="employees">
         {
             //the key needs to be where you are rendering multiple components
             //the props are what im pulling out of the employeeArray
-            employees.map(employee => {
-                return <section className="employee" key={employee.id}>
-                    <header className="employee__header">{employee?.user?.name}</header>
-                <div>Start Date:  {employee?.startDate}</div>
-                <div>Pay Rate:  {employee?.payRate}</div>
-                    <footer className="employee__footer">Currently working at {employee?.location?.address}</footer>
-                    </section>
-            }
+            employees.map(employee => <Employee key={`employee--${employee.id}`} 
+            employeeObject={employee} 
+            getAllEmployees={getAllEmployees}/>
         )}
     
     </article>
